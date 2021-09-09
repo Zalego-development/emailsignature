bc2dce56b4c498a74838f081c50637f986b12b50
seffu <seffukioi@gmail.com>
2021-09-09 15:55:58 +0300

    sampleone

--- signature/views.py
+++ signature/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 import json
 import requests
+import base64
 from requests.structures import CaseInsensitiveDict
 
 def get_data():
@@ -37,13 +38,40 @@ def index(request):
     return render(request, 'signature/index.html',{'employee':employee})
 
 
+# def get_emp(request,empid):
+#     id = empid
+#     employees = get_data()
+
+#     for employee in employees:
+#         if employee['Employee Id'] == id:
+#             emp_data = {
+#                 'full_name' : employee['Full Name'],
+#                 'employee_id' : employee['Employee Id'],
+#                 'company' : employee['Company'],
+#                 'designation' : employee['Designation'],
+#                 'department' : employee['Department'],
+#                 'job_title' : employee['Job Title'],
+#                 'phone_number' : employee['Phone Number'],
+#                 'email' : employee['Email'],
+#                 # 'location' : employee['Loacation'],
+#                 'photo' : employee['photo']   
+#             }
+#     context = {'employee': emp_data}
+#     return render(request, 'signature/index.html',context)
+
+
 def get_emp(request,empid):
     id = empid
     employees = get_data()
-
     for employee in employees:
         if employee['Employee Id'] == id:
-            print(employee['Full Name'])
+            
+            URL = employee['photo']
+            encoded_string = base64.encodestring(requests.get(URL).content)
+            if URL.endswith('.jpg') or URL.endswith('.jpeg'):
+                start_code = "data:image/jpeg;base64,"
+            else:
+                start_code = "data:image/png;base64,"
             emp_data = {
                 'full_name' : employee['Full Name'],
                 'employee_id' : employee['Employee Id'],
@@ -54,7 +82,30 @@ def get_emp(request,empid):
                 'phone_number' : employee['Phone Number'],
                 'email' : employee['Email'],
                 # 'location' : employee['Loacation'],
-                'photo' : employee['photo']   
+                # 'photo' : employee['photo'],
+                # # 'photo' : encoded_string.decode("utf-8"),
+                # 'start_code': start_code,
+                # 'fullphoto':start_code+encoded_string.decode("utf-8"),
             }
-    context = {'employee': emp_data}
+            if employee['Company'] == 'Zalda Company':
+                companyLogo = "https://github.com/Zalego-development/CompanyLogos/blob/main/zaldacropped.png?raw=true"
+                website = "www.zalda.com"
+            elif employee['Company'] == 'Zalego Academy':
+                companyLogo = "https://github.com/Zalego-development/CompanyLogos/blob/main/zalegocropped.png?raw=true"
+                website = "www.zalegoacademy.ac.ke"
+            elif employee['Company'] == 'DAPROIM AFRICA LIMITED' or employee['Company'] == 'StepWise':
+                companyLogo = "https://github.com/Zalego-development/CompanyLogos/blob/main/stepwisecropped.png?raw=true"
+                website = "stepwise.net"
+            elif employee['Company'] == 'NEXT STEP FOUNDATION':
+                companyLogo = "https://github.com/Zalego-development/CompanyLogos/blob/main/nextcropped.png?raw=true"
+
+            if employee['photo'][-9:] == 'admin.png':
+                photo = "https://github.com/Zalego-development/CompanyLogos/blob/main/placeholder-hi.png?raw=true"
+            else:
+                photo = employee['photo']
+                
+            print(employee['photo'][-9:])
+            context = {'employee': emp_data, 'companyLogo':companyLogo, 'website':website, 'photo':photo}
+            # context = {'employee': emp_data}
+    
     return render(request, 'signature/index.html',context)
